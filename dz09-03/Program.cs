40e6335b5f424ce17e2be8d6320e636df52b834e
Pavel Starodubov <pablostar68@yahoo.com>
2022-11-15 20:52:13 +0300

    add dz09-03

---
+++ dz09-03/Program.cs
@@ -0,0 +1,23 @@
+﻿// Написать программу вычисления функции Аккермана
+
+
+int Ackerman(int m, int n)
+{
+    if (m == 0)
+        return n + 1;
+    if (m > 0 && n == 0)
+        return Ackerman(m - 1, 1);
+    if (m > 0 && n > 0)
+        return Ackerman(m - 1, Ackerman(m, n - 1));
+    return Ackerman(m, n);
+}
+
+Console.WriteLine("Введите m: ");
+int m = int.Parse(Console.ReadLine() ?? "0");
+Console.WriteLine("Введите n: ");
+int n = int.Parse(Console.ReadLine() ?? "0");
+Console.WriteLine();
+
+int ackermanFunction = Ackerman(m, n);
+Console.WriteLine($"Вывод: {ackermanFunction}");
+
