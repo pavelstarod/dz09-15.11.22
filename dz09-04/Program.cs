ec042a0a74ff61f95ec2b2acdd929931075c7b1f
Pavel Starodubov <pablostar68@yahoo.com>
2022-11-16 21:21:15 +0300

    add dz09-04

---
+++ dz09-04/Program.cs
@@ -0,0 +1,25 @@
+﻿// Написать программу показывающие первые N чисел, для которых каждое следующее равно сумме двух предыдущих. 
+// Первые два элемента последовательности задаются пользователем
+
+
+Console.WriteLine("Первое число: ");
+int a = int.Parse(Console.ReadLine() ?? "0");
+Console.WriteLine("Второе число: ");
+int b = int.Parse(Console.ReadLine() ?? "0");
+Console.WriteLine("Количество чисел: ");
+int N = int.Parse(Console.ReadLine() ?? "0");
+Console.WriteLine("Вывод последовательности: ");
+
+Console.Write($"{a} {b} ");
+
+for (int i = 0; i < N-2; i++)
+{
+    int result = a + b;
+    a = b;
+    b = result;
+    Console.Write($"{result} ");
+}
+
+
+
+
